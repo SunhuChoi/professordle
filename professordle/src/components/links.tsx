3a28dc6d5193cdac261c2ed80bc77281ad2e8578
unknown <sunhu@ucsb.edu>
2024-01-13 11:22:49 -0800

    Testing

---
+++ professordle/src/components/links.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+
+export const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
